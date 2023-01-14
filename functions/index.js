233c00526e4e62f59a6590152d671f1a3031adbb
kaf-lamed-beyt <belac335@gmail.com>
2023-01-14 16:50:23 +0100

    fix: function module import

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const {
   createProbot,
 } = require("@probot/adapter-aws-lambda-serverless");
 
-const app = require("../../../index");
+const app = require("../index");
 
 module.exports = createNodeMiddleware(app, {
   probot: createProbot(),
