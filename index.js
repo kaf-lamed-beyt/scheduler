4ed1434e57cd66c5264da049c50a72b14540d9a8
kaf-lamed-beyt <belac335@gmail.com>
2023-01-15 23:02:12 +0100

    chore: refactor module export:

--- index.js
+++ index.js
@@ -4,72 +4,72 @@
  */
 const moment = require("moment");
 
-app.on("issue_comment.created", async (context) => {
-  // Extract the username, "merge" keyword, and date format from the comment body
-  const comment = context.payload.comment.body;
-  const username = context.payload.comment.user.login;
-  const mergeKeyword = "merge";
-  const dateFormat = /\d{4}-\d{2}-\d{2}/;
+module.exports = (app) => {
+  app.on("issue_comment.created", async (context) => {
+    // Extract the username, "merge" keyword, and date format from the comment body
+    const comment = context.payload.comment.body;
+    const username = context.payload.comment.user.login;
+    const mergeKeyword = "merge";
+    const dateFormat = /\d{4}-\d{2}-\d{2}/;
 
-  if (
-    comment.includes(`@${app.appName}`) &&
-    comment.includes(mergeKeyword) &&
-    comment.match(dateFormat)
-  ) {
-    // Extract the scheduled date from the comment and parse it using moment
-    const scheduledDate = moment(comment.match(dateFormat)[0]);
-    // Check if the scheduled date is in the future
-    if (scheduledDate.isAfter(moment())) {
-      // Check if the user has permission to merge pull requests in the repository
-      const repo = context.payload.repository;
-      const { data: collaborators } =
-        await context.github.repos.listCollaborators({
-          owner: repo.owner.login,
-          repo: repo.name,
-        });
-      const hasPermission = collaborators.find(
-        (collaborator) => collaborator.login === username
-      );
-      if (hasPermission) {
-        // Respond with a comment confirming that the merge request has been scheduled
-        await context.github.issues.createComment(
-          context.issue({
-            body: `Hey @${username}, your merge request has been scheduled for ${scheduledDate.format(
-              "YYYY-MM-DD"
-            )}`,
-          })
+    if (
+      comment.includes(`@${app.appName}`) &&
+      comment.includes(mergeKeyword) &&
+      comment.match(dateFormat)
+    ) {
+      // Extract the scheduled date from the comment and parse it using moment
+      const scheduledDate = moment(comment.match(dateFormat)[0]);
+      // Check if the scheduled date is in the future
+      if (scheduledDate.isAfter(moment())) {
+        // Check if the user has permission to merge pull requests in the repository
+        const repo = context.payload.repository;
+        const { data: collaborators } =
+          await context.github.repos.listCollaborators({
+            owner: repo.owner.login,
+            repo: repo.name,
+          });
+        const hasPermission = collaborators.find(
+          (collaborator) => collaborator.login === username
         );
-        // Schedule the merge request
-        scheduleMergeRequest(context, scheduledDate);
+        if (hasPermission) {
+          // Respond with a comment confirming that the merge request has been scheduled
+          await context.github.issues.createComment(
+            context.issue({
+              body: `Hey @${username}, your merge request has been scheduled for ${scheduledDate.format(
+                "YYYY-MM-DD"
+              )}`,
+            })
+          );
+          // Schedule the merge request
+          scheduleMergeRequest(context, scheduledDate);
+        } else {
+          // Respond with a comment telling the user they do not have permission
+          await context.github.issues.createComment(
+            context.issue({
+              body: `Hey @${username}, you do not have permission to merge pull requests in this repository`,
+            })
+          );
+        }
       } else {
-        // Respond with a comment telling the user they do not have permission
         await context.github.issues.createComment(
           context.issue({
-            body: `Hey @${username}, you do not have permission to merge pull requests in this repository`,
+            body: `Hey @${username}, the date is not in the future.`,
           })
         );
       }
-    } else {
-      await context.github.issues.createComment(
-        context.issue({
-          body: `Hey @${username}, the date is not in the future.`,
-        })
-      );
     }
-  }
-});
+  });
 
-const scheduleMergeRequest = async (context, scheduledDate) => {
-  var waitTime = scheduledDate.diff(moment());
-  setTimeout(async () => {
-    // Check if the pull request is still open
-    const pr = context.payload.pull_request;
-    if (pr.state === "open") {
-      // Merge the pull request
-      await context.github.pulls.merge(context.pullRequest({}));
-      console.log("Merged at :", moment().format());
-    }
-  }, waitTime);
+  const scheduleMergeRequest = async (context, scheduledDate) => {
+    var waitTime = scheduledDate.diff(moment());
+    setTimeout(async () => {
+      // Check if the pull request is still open
+      const pr = context.payload.pull_request;
+      if (pr.state === "open") {
+        // Merge the pull request
+        await context.github.pulls.merge(context.pullRequest({}));
+        console.log("Merged at :", moment().format());
+      }
+    }, waitTime);
+  };
 };
-
-module.exports = app;
