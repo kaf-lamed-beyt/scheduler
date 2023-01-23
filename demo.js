c89d502b2483c605e57df5cbdbcfe2d6ade6a174
kaf-lamed-beyt <belac335@gmail.com>
2023-01-23 13:53:56 +0100

    refactor demo instance of the bot

--- demo.js
+++ demo.js
@@ -1,10 +1,12 @@
 /**
- * This is the main entrypoint to my Probot app
  * @param {import('probot').Probot} app
  */
 
+const moment = require("moment");
 const APP_NAME = "agba-merger";
-const MERGE_KEYWORD = "please merge this pull request";
+const MERGE_KEYWORD = "merge";
+
+let ERROR_MESSAGE_STATE = false;
 
 module.exports = (app) => {
   app.on("issue_comment.created", async (context) => {
@@ -14,41 +16,22 @@ module.exports = (app) => {
       const AUTHOR_ROLE = context.payload.issue.author_association;
       const ISSUE_NUMBER = context.issue.number;
 
-      if (COMMENT.includes(`@${APP_NAME}`) && COMMENT.includes(MERGE_KEYWORD)) {
+      if (
+        COMMENT.includes(`@${APP_NAME}`) &&
+        COMMENT.includes(MERGE_KEYWORD)
+      ) {
         if (AUTHOR_ROLE === "OWNER" || AUTHOR_ROLE === "COLLABORATOR") {
-          await context.octokit.issues.createComment(
-            context.issue({
-              body: `Hi @${USERNAME}, your merge request is being processed...`,
-            })
-          );
-
-          const pull_request = await context.octokit.pulls.get({
+          await context.octokit.pulls.merge({
             owner: context.payload.repository.owner.login,
             repo: context.payload.repository.name,
             pull_number: ISSUE_NUMBER,
           });
 
-          if (pull_request.data.state === "open") {
-            const merge = await context.octokit.pulls.merge({
-              owner: context.payload.repository.owner.login,
-              repo: context.payload.repository.name,
-              pull_number: ISSUE_NUMBER,
-            });
-
-            if (merge.status === 204) {
-              await context.octokit.issues.createComment(
-                context.issue({
-                  body: `Hi @${USERNAME}, your pull request was successfully merged`,
-                })
-              );
-            }
-          } else {
-            await context.octokit.issues.createComment(
-              context.issue({
-                body: `Hi @${USERNAME}, your pull request could not be merged as it is not in the open state`,
-              })
-            );
-          }
+          await context.octokit.issues.createComment(
+            context.issue({
+              body: `Hi @${USERNAME}, your merge request has been successfully merged`,
+            })
+          );
         } else {
           await context.octokit.issues.createComment(
             context.issue({
@@ -57,11 +40,22 @@ module.exports = (app) => {
           );
         }
       }
+
+      if (
+        !COMMENT.includes(MERGE_KEYWORD)
+      ) {
+        await context.octokit.issues.createComment(
+          context.issue({
+            body: `Hi @${USERNAME}, you need to pass the "merge" keyword  to merge the pull request`,
+          })
+        );
+      }
     } catch (error) {
       console.error(error);
+
       await context.octokit.issues.createComment(
         context.issue({
-          body: `Hi @${USERNAME}, something went wrong while trying to merge your pull request. Please try again later or contact an admin for assistance.`,
+          body: `@${USERNAME}, something went wrong while I tried merging your pull request. Please try again later or contact an admin for assistance.`,
         })
       );
     }
