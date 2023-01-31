cc4c67a9406ca658f46f2e7a299791223614ce22
kaf-lamed-beyt <belac335@gmail.com>
2023-01-31 23:31:35 +0100

    chore: update cron schedule

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ module.exports = (app) => {
       const COMMENT = context.payload.comment.body;
       const USERNAME = context.payload.comment.user.login;
       const AUTHOR_ROLE = context.payload.issue.author_association;
-      const ISSUE_NUMBER = context.issue.number;
 
       const scheduledDateMatch = COMMENT.match(/(\d{4}-\d{2}-\d{2})/);
       const scheduledTimeMatch = COMMENT.match(/(\d{2}:\d{2})/);
