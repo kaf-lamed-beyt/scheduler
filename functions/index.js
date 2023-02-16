88c2b02c505148a8e15d18685ec1bafb535c82d6
kaf-lamed-beyt <belac335@gmail.com>
2023-02-16 09:11:04 +0100

    chore: move to netlify functions

---
+++ functions/index.js
@@ -0,0 +1,9 @@
+const {
+  createLambdaFunction,
+  createProbot,
+} = require("@probot/adapter-aws-lambda-serverless");
+const appFn = require("../index");
+
+module.exports.handler = createLambdaFunction(appFn, {
+  probot: createProbot(),
+});
