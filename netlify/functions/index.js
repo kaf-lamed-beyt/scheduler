30964313040aff2be488ba95f475d8bdc60d20c6
kaf-lamed-beyt <belac335@gmail.com>
2023-02-16 09:28:56 +0100

    fix: proper function naming

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -2,7 +2,7 @@ const {
   createLambdaFunction,
   createProbot,
 } = require("@probot/adapter-aws-lambda-serverless");
-const appFn = require("../index");
+const appFn = require("../../index");
 
 exports.handler = createLambdaFunction(appFn, {
   probot: createProbot(),
