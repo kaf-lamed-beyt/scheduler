1fa25d190502532ed3b54d2df83174338d68da22
kaf-lamed-beyt <belac335@gmail.com>
2023-02-16 09:24:17 +0100

    fix: function handler export

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,6 @@ const {
 } = require("@probot/adapter-aws-lambda-serverless");
 const appFn = require("../index");
 
-module.exports.handler = createLambdaFunction(appFn, {
+exports.handler = createLambdaFunction(appFn, {
   probot: createProbot(),
 });
